853b1da45e9a160436d23b9429022b4621bbd567
Yuki TAKEDA <teakun@kittengine.com>
2018-07-16 16:11:17 +0900

    Initial Commit

---
+++ SekigaeKun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SekigaeKun
+//
+//  Created by Yuki Takeda on 2018/07/16.
+//  Copyright © 2018年 TAKEDA Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
